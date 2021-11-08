6a97619ede4f784f1a28281fff350a76c694cae8
Steven Penny <srpen6@gmail.com>
2021-11-08 12:43:33 -0600

    +func (a Auth) Delivery(dev *Device, app string, ver int) (protobuf.Decoder, error) {

--- proto.go
+++ proto.go
@@ -9,7 +9,7 @@ import (
    "strconv"
 )
 
-func (a Auth) Delivery(dev *Device, app string, ver int) (protobuf.Message, error) {
+func (a Auth) Delivery(dev *Device, app string, ver int) (protobuf.Decoder, error) {
    req, err := http.NewRequest("GET", origin + "/fdfe/delivery", nil)
    if err != nil {
       return nil, err
@@ -32,68 +32,10 @@ func (a Auth) Delivery(dev *Device, app string, ver int) (protobuf.Message, erro
    if err != nil {
       return nil, err
    }
-   return protobuf.Parse(buf), nil
+   return protobuf.NewDecoder(buf), nil
 }
 
-// This seems to return `StatusOK`, even with invalid requests, and the response
-// body only contains a token, that doesnt seem to indicate success or failure.
-// Only way I know to check, it to try the `deviceID` with a `details` request
-// or similar. Also, after the POST, you need to wait at least 16 seconds
-// before the `deviceID` can be used.
-func (a Auth) Upload(dev *Device, config protobuf.Message) error {
-   buf := config.Marshal()
-   req, err := http.NewRequest(
-      "POST", origin + "/fdfe/uploadDeviceConfig", bytes.NewReader(buf),
-   )
-   if err != nil {
-      return err
-   }
-   req.Header = http.Header{
-      "Authorization": {"Bearer " + a.Get("Auth")},
-      "User-Agent": {agent},
-      "X-DFE-Device-ID": {dev.String()},
-   }
-   res, err := roundTrip(req)
-   if err != nil {
-      return err
-   }
-   return res.Body.Close()
-}
-
-var DefaultConfig = protobuf.Message{
-   deviceConfiguration: protobuf.Message{
-      touchScreen: int32(1),
-      keyboard: int32(1),
-      navigation: int32(1),
-      screenLayout: int32(1),
-      hasHardKeyboard: true,
-      hasFiveWayNavigation: true,
-      screenDensity: int32(1),
-      // developer.android.com/guide/topics/manifest/uses-feature-element
-      glEsVersion: int32(0x0009_0000),
-      // developer.android.com/guide/topics/manifest/uses-feature-element
-      systemAvailableFeature: protobuf.Repeated{
-         // com.pinterest
-         "android.hardware.camera",
-         // com.pinterest
-         "android.hardware.faketouch",
-         // com.pinterest
-         "android.hardware.location",
-         // com.pinterest
-         "android.hardware.screen.portrait",
-         // com.google.android.youtube
-         "android.hardware.touchscreen",
-         // com.google.android.youtube
-         "android.hardware.wifi",
-      },
-      // developer.android.com/ndk/guides/abis
-      nativePlatform: protobuf.Repeated{
-         "armeabi-v7a",
-      },
-   },
-}
-
-func (a Auth) Details(dev *Device, app string) (protobuf.Message, error) {
+func (a Auth) Details(dev *Device, app string) (protobuf.Decoder, error) {
    req, err := http.NewRequest("GET", origin + "/fdfe/details", nil)
    if err != nil {
       return nil, err
@@ -114,45 +56,87 @@ func (a Auth) Details(dev *Device, app string) (protobuf.Message, error) {
    if err != nil {
       return nil, err
    }
-   return protobuf.Parse(buf), nil
+   return protobuf.NewDecoder(buf), nil
 }
 
-type responseWrapper struct {
-   protobuf.Message
+type Config struct {
+   DeviceConfiguration DeviceConfiguration `json:"1"`
 }
 
-func (r responseWrapper) payload() protobuf.Message {
-   return r.Message.Message(1)
+type DeviceConfiguration struct {
+   TouchScreen int32 `json:"1"`
+   Keyboard int32 `json:"2"`
+   Navigation int32 `json:"3"`
+   ScreenLayout int32 `json:"4"`
+   HasHardKeyboard bool `json:"5"`
+   HasFiveWayNavigation bool `json:"6"`
+   ScreenDensity int32 `json:"7"`
+   GlEsVersion int32 `json:"8"`
+   SystemAvailableFeature []string `json:"10"`
+   NativePlatform []string `json:"11"`
 }
 
-type payload struct {
-   protobuf.Message
+// This seems to return `StatusOK`, even with invalid requests, and the response
+// body only contains a token, that doesnt seem to indicate success or failure.
+// Only way I know to check, it to try the `deviceID` with a `details` request
+// or similar. Also, after the POST, you need to wait at least 16 seconds
+// before the `deviceID` can be used.
+func (a Auth) Upload(dev *Device, con Config) error {
+   enc, err := protobuf.NewEncoder(con)
+   if err != nil {
+      return err
+   }
+   buf, err := enc.Encode()
+   if err != nil {
+      return err
+   }
+   req, err := http.NewRequest(
+      "POST", origin + "/fdfe/uploadDeviceConfig", bytes.NewReader(buf),
+   )
+   if err != nil {
+      return err
+   }
+   req.Header = http.Header{
+      "Authorization": {"Bearer " + a.Get("Auth")},
+      "User-Agent": {agent},
+      "X-DFE-Device-ID": {dev.String()},
+   }
+   res, err := roundTrip(req)
+   if err != nil {
+      return err
+   }
+   return res.Body.Close()
 }
 
-func (p payload) detailsResponse() protobuf.Message {
-   return p.Message.Message(2)
-}
-
-type detailsResponse struct {
-   protobuf.Message
-}
-
-func (d detailsResponse) docV2() protobuf.Message {
-   return d.Message.Message(4)
-}
-
-type docV2 struct {
-   protobuf.Message
-}
-
-func (d docV2) details() protobuf.Message {
-   return d.Message.Message(13)
-}
-
-type documentDetails struct {
-   protobuf.Message
-}
-
-func (d documentDetails) appDetails() protobuf.Message {
-   return d.Message.Message(1)
+var DefaultConfig = Config{
+   DeviceConfiguration: DeviceConfiguration{
+      TouchScreen: 1,
+      Keyboard: 1,
+      Navigation: 1,
+      ScreenLayout: 1,
+      HasHardKeyboard: true,
+      HasFiveWayNavigation: true,
+      ScreenDensity: 1,
+      // developer.android.com/guide/topics/manifest/uses-feature-element
+      GlEsVersion: 0x0009_0000,
+      // developer.android.com/guide/topics/manifest/uses-feature-element
+      SystemAvailableFeature: []string{
+         // com.pinterest
+         "android.hardware.camera",
+         // com.pinterest
+         "android.hardware.faketouch",
+         // com.pinterest
+         "android.hardware.location",
+         // com.pinterest
+         "android.hardware.screen.portrait",
+         // com.google.android.youtube
+         "android.hardware.touchscreen",
+         // com.google.android.youtube
+         "android.hardware.wifi",
+      },
+      // developer.android.com/ndk/guides/abis
+      NativePlatform: []string{
+         "armeabi-v7a",
+      },
+   },
 }
