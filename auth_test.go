e70a69bac292e5c95fe1e2e0118497756f0053fa
Steven Penny <srpen6@gmail.com>
2021-11-18 07:30:22 -0600

    new file:   research/2021-11-17/readme.md

--- auth_test.go
+++ auth_test.go
@@ -4,32 +4,17 @@ import (
    "fmt"
    "os"
    "testing"
+   "time"
 )
 
-const (
-   app = "com.pbs.video"
-   ver = 510565
-)
-
-func TestDelivery(t *testing.T) {
-   auth, cache, err := getAuth()
-   if err != nil {
-      t.Fatal(err)
-   }
-   dev := new(Device)
-   r, err := os.Open(cache + "/googleplay/device.json")
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer r.Close()
-   if err := dev.Decode(r); err != nil {
-      t.Fatal(err)
-   }
-   del, err := auth.Delivery(dev, app, ver)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%+v\n", del)
+var apps = map[string]int{
+   "com.axis.drawingdesk.v3": 190,
+   "com.google.android.youtube": 1524221376,
+   "com.instagram.android": 321403734,
+   "com.pinterest": 9398020,
+   "com.smarty.voomvoom": 369,
+   "com.vimeo.android.videoapp": 3510004,
+   "org.videolan.vlc": 13040207,
 }
 
 func TestDetails(t *testing.T) {
@@ -46,11 +31,40 @@ func TestDetails(t *testing.T) {
    if err := dev.Decode(r); err != nil {
       t.Fatal(err)
    }
-   det, err := auth.Details(dev, app)
+   for app := range apps {
+      det, err := auth.Details(dev, app)
+      if err != nil {
+         t.Fatal(err)
+      }
+      if det.DocV2.Details.AppDetails.VersionCode == 0 {
+         t.Fatal(app)
+      }
+      fmt.Println(det.DocV2.Details.AppDetails.VersionCode)
+      fmt.Println(det.DocV2.Details.AppDetails.InstallationSize)
+      time.Sleep(time.Second)
+   }
+}
+
+func TestDelivery(t *testing.T) {
+   auth, cache, err := getAuth()
    if err != nil {
       t.Fatal(err)
    }
-   fmt.Printf("%+v\n", det)
+   dev := new(Device)
+   r, err := os.Open(cache + "/googleplay/device.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer r.Close()
+   if err := dev.Decode(r); err != nil {
+      t.Fatal(err)
+   }
+   app := "org.videolan.vlc"
+   del, err := auth.Delivery(dev, app, apps[app])
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", del)
 }
 
 func getAuth() (*Auth, string, error) {
---
+++ research/2021-11-17/readme.md
@@ -0,0 +1,3 @@
+# November 17 2021
+
+https://github.com/httptoolkit/frida-android-unpinning/issues/8
