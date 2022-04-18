36f7d39376c5d54b2ebf2439036d073658b606bb
Zombo <srpen6@gmail.com>
2022-04-18 09:07:12 -0500

    +func TestDetails(t *testing.T) {

--- header_test.go
+++ header_test.go
@@ -7,95 +7,71 @@ import (
    "time"
 )
 
-var appsX86 = []app{
-   {"Apr 11, 2022", "com.instagram.android"},
-   {"Apr 11, 2022", "com.pinterest"},
-   {"Apr 12, 2022", "br.com.rodrigokolb.realdrum"},
-   {"Apr 4, 2022", "com.vimeo.android.videoapp"},
-   {"Apr 6, 2022", "org.thoughtcrime.securesms"},
-   {"Apr 7, 2022", "com.google.android.youtube"},
-   {"Feb 14, 2022", "org.videolan.vlc"},
-   {"Jun 1, 2021", "com.valvesoftware.android.steam.community"},
-   {"Mar 1, 2022", "kr.sira.metal"},
-   {"Mar 17, 2022", "com.google.android.apps.walletnfcrel"},
-   {"Mar 21, 2022", "com.jackpocket"},
-   {"May 22, 2021", "com.smarty.voomvoom"},
-}
-
-var appsArmeabi = []app{
-   {"Apr 8, 2022", "com.axis.drawingdesk.v3"},
-   {"Mar 14, 2022", "com.xiaomi.smarthome"},
-   {"Mar 24, 2022", "com.miui.weather2"},
-}
-
-var appsArm64 = []app{
-   {"Mar 30, 2022", "com.exnoa.misttraingirls"},
-}
-
-func TestDetailsArm64(t *testing.T) {
-   err := testDetails("googleplay/arm64.json", appsArm64)
-   if err != nil {
-      t.Fatal(err)
+func TestDetails(t *testing.T) {
+   for elem, apps := range testApps {
+      head, err := newHeader(elem)
+      if err != nil {
+         t.Fatal(err)
+      }
+      for _, app := range apps {
+         det, err := head.Details(app.id)
+         if err != nil {
+            t.Fatal(err)
+         }
+         if det.CurrencyCode == "" {
+            t.Fatal(det)
+         }
+         if det.NumDownloads == 0 {
+            t.Fatal(det)
+         }
+         if det.Size == 0 {
+            t.Fatal(det)
+         }
+         if det.Title == "" {
+            t.Fatal(det)
+         }
+         if det.UploadDate == "" {
+            t.Fatal(det)
+         }
+         if det.VersionCode == 0 {
+            t.Fatal(det)
+         }
+         if det.VersionString == "" {
+            t.Fatal(det)
+         }
+         time.Sleep(99 * time.Millisecond)
+      }
    }
 }
 
-func TestDetailsArmeabi(t *testing.T) {
-   err := testDetails("googleplay/armeabi.json", appsArmeabi)
-   if err != nil {
-      t.Fatal(err)
-   }
-}
-
-func TestDetailsX86(t *testing.T) {
-   err := testDetails("googleplay/x86.json", appsX86)
-   if err != nil {
-      t.Fatal(err)
-   }
+var testApps = map[string][]app{
+   "googleplay/arm64.json": {
+      {"Mar 30, 2022", "com.exnoa.misttraingirls"},
+   },
+   "googleplay/armeabi.json": {
+      {"Apr 8, 2022", "com.axis.drawingdesk.v3"},
+      {"Mar 14, 2022", "com.xiaomi.smarthome"},
+      {"Mar 24, 2022", "com.miui.weather2"},
+   },
+   "googleplay/x86.json": {
+      {"Apr 11, 2022", "com.instagram.android"},
+      {"Apr 11, 2022", "com.pinterest"},
+      {"Apr 12, 2022", "br.com.rodrigokolb.realdrum"},
+      {"Apr 4, 2022", "com.vimeo.android.videoapp"},
+      {"Apr 6, 2022", "org.thoughtcrime.securesms"},
+      {"Apr 7, 2022", "com.google.android.youtube"},
+      {"Feb 14, 2022", "org.videolan.vlc"},
+      {"Jun 1, 2021", "com.valvesoftware.android.steam.community"},
+      {"Mar 1, 2022", "kr.sira.metal"},
+      {"Mar 17, 2022", "com.google.android.apps.walletnfcrel"},
+      {"Mar 21, 2022", "com.jackpocket"},
+      {"May 22, 2021", "com.smarty.voomvoom"},
+   },
 }
 
 type app struct {
    date string
    id string
-}
-
-func (a app) Error() string {
-   return a.id
-}
-
-func testDetails(device string, apps []app) error {
-   head, err := newHeader(device)
-   if err != nil {
-      return err
-   }
-   for _, app := range apps {
-      det, err := head.Details(app.id)
-      if err != nil {
-         return err
-      }
-      if det.CurrencyCode == "" {
-         return app
-      }
-      if det.NumDownloads == 0 {
-         return app
-      }
-      if det.Size == 0 {
-         return app
-      }
-      if det.Title == "" {
-         return app
-      }
-      if det.UploadDate == "" {
-         return app
-      }
-      if det.VersionCode == 0 {
-         return app
-      }
-      if det.VersionString == "" {
-         return app
-      }
-      time.Sleep(99 * time.Millisecond)
-   }
-   return nil
 }
 
 func TestDelivery(t *testing.T) {
