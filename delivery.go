c05402b1011dc1d42aef91c1d951b13f1664428b
Zombo <srpen6@gmail.com>
2022-05-20 21:19:10 -0500

    new file:   config.go

---
+++ delivery.go
@@ -0,0 +1,141 @@
+package googleplay
+
+import (
+   "errors"
+   "github.com/89z/format/protobuf"
+   "net/http"
+   "net/url"
+   "strconv"
+)
+
+func (h Header) Delivery(app string, ver uint64) (*Delivery, error) {
+   req, err := http.NewRequest(
+      "GET", "https://play-fe.googleapis.com/fdfe/delivery", nil,
+   )
+   if err != nil {
+      return nil, err
+   }
+   h.SetAgent(req.Header)
+   h.SetDevice(req.Header)
+   req.URL.RawQuery = url.Values{
+      "doc": {app},
+      "vc": {strconv.FormatUint(ver, 10)},
+   }.Encode()
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   responseWrapper, err := protobuf.Decode(res.Body)
+   if err != nil {
+      return nil, err
+   }
+   // .payload.deliveryResponse.status
+   status, err := responseWrapper.Get(1).Get(21).GetVarint(1)
+   if err != nil {
+      return nil, err
+   }
+   switch status {
+   case 2:
+      return nil, errors.New("geo-blocking")
+   case 3:
+      return nil, errors.New("purchase required")
+   case 5:
+      return nil, errors.New("invalid version")
+   }
+   // .payload.deliveryResponse.appDeliveryData
+   appData := responseWrapper.Get(1).Get(21).Get(2)
+   var del Delivery
+   // .downloadUrl
+   del.DownloadURL, err = appData.GetString(3)
+   if err != nil {
+      return nil, err
+   }
+   del.PackageName = app
+   del.VersionCode = ver
+   // .splitDeliveryData
+   for _, data := range appData.GetMessages(15) {
+      var split SplitDeliveryData
+      // .id
+      split.ID, err = data.GetString(1)
+      if err != nil {
+         return nil, err
+      }
+      // .downloadUrl
+      split.DownloadURL, err = data.GetString(5)
+      if err != nil {
+         return nil, err
+      }
+      del.SplitDeliveryData = append(del.SplitDeliveryData, split)
+   }
+   // .additionalFile
+   for _, file := range appData.GetMessages(4) {
+      var app AppFileMetadata
+      // .fileType
+      app.FileType, err = file.GetVarint(1)
+      if err != nil {
+         return nil, err
+      }
+      // .downloadUrl
+      app.DownloadURL, err = file.GetString(4)
+      if err != nil {
+         return nil, err
+      }
+      del.AdditionalFile = append(del.AdditionalFile, app)
+   }
+   return &del, nil
+}
+
+type AppFileMetadata struct {
+   FileType Varint
+   DownloadURL String
+}
+
+func (d Delivery) Additional(typ Varint) string {
+   var buf []byte
+   if typ == 0 {
+      buf = append(buf, "main"...)
+   } else {
+      buf = append(buf, "patch"...)
+   }
+   buf = append(buf, '.')
+   buf = strconv.AppendUint(buf, d.VersionCode, 10)
+   buf = append(buf, '.')
+   buf = append(buf, d.PackageName...)
+   buf = append(buf, ".obb"...)
+   return string(buf)
+}
+
+type Delivery struct {
+   DownloadURL String
+   PackageName string
+   SplitDeliveryData []SplitDeliveryData
+   VersionCode uint64
+   AdditionalFile []AppFileMetadata
+}
+
+type SplitDeliveryData struct {
+   ID String
+   DownloadURL String
+}
+
+func (d Delivery) Split(id String) string {
+   var buf []byte
+   buf = append(buf, d.PackageName...)
+   buf = append(buf, '-')
+   buf = append(buf, id...)
+   buf = append(buf, '-')
+   buf = strconv.AppendUint(buf, d.VersionCode, 10)
+   buf = append(buf, ".apk"...)
+   return string(buf)
+}
+
+func (d Delivery) Download() string {
+   var buf []byte
+   buf = append(buf, d.PackageName...)
+   buf = append(buf, '-')
+   buf = strconv.AppendUint(buf, d.VersionCode, 10)
+   buf = append(buf, ".apk"...)
+   return string(buf)
+}
--- play.go
+++ play.go
@@ -2,7 +2,6 @@ package googleplay
 
 import (
    "bufio"
-   "bytes"
    "errors"
    "github.com/89z/format"
    "github.com/89z/format/crypto"
@@ -15,116 +14,91 @@ import (
    "time"
 )
 
-const (
-   DateInput = "Jan 2, 2006"
-   DateOutput = "2006-01-02"
-)
-
-type Details struct {
-   Title String
-   Creator String
-   UploadDate String // Jun 1, 2021
-   VersionString String
-   VersionCode Varint
-   NumDownloads Varint
-   Size Varint
-   File []Varint
-   Micros Varint
-   CurrencyCode String
+func (h Header) SetAuth(head http.Header) {
+   head.Set("Authorization", "Bearer " + h.Auth)
 }
 
-func (d Details) String() string {
-   var buf []byte
-   buf = append(buf, "Title: "...)
-   buf = append(buf, d.Title...)
-   buf = append(buf, "\nCreator: "...)
-   buf = append(buf, d.Creator...)
-   buf = append(buf, "\nUploadDate: "...)
-   buf = append(buf, d.UploadDate...)
-   buf = append(buf, "\nVersionString: "...)
-   buf = append(buf, d.VersionString...)
-   buf = append(buf, "\nVersionCode: "...)
-   buf = strconv.AppendUint(buf, uint64(d.VersionCode), 10)
-   buf = append(buf, "\nNumDownloads: "...)
-   buf = append(buf, format.LabelNumber(d.NumDownloads)...)
-   buf = append(buf, "\nSize: "...)
-   buf = append(buf, format.LabelSize(d.Size)...)
-   buf = append(buf, "\nFile:"...)
-   for _, file := range d.File {
-      if file == 0 {
-         buf = append(buf, " APK"...)
-      } else {
-         buf = append(buf, " OBB"...)
-      }
-   }
-   buf = append(buf, "\nOffer: "...)
-   buf = strconv.AppendUint(buf, uint64(d.Micros), 10)
-   buf = append(buf, ' ')
-   buf = append(buf, d.CurrencyCode...)
-   return string(buf)
+func (h Header) SetDevice(head http.Header) {
+   device := strconv.FormatUint(h.AndroidID, 16)
+   head.Set("X-DFE-Device-ID", device)
 }
 
-type deviceConfiguration struct {
-   app string
-}
-
-func (d deviceConfiguration) Error() string {
-   var buf []byte
-   buf = append(buf, "bad DeviceConfiguration for "...)
-   buf = strconv.AppendQuote(buf, d.app)
-   return string(buf)
-}
-type AppFileMetadata struct {
-   FileType Varint
-   DownloadURL String
-}
-
-func (d Delivery) Additional(typ Varint) string {
-   var buf []byte
-   if typ == 0 {
-      buf = append(buf, "main"...)
-   } else {
-      buf = append(buf, "patch"...)
-   }
-   buf = append(buf, '.')
-   buf = strconv.AppendUint(buf, d.VersionCode, 10)
-   buf = append(buf, '.')
-   buf = append(buf, d.PackageName...)
-   buf = append(buf, ".obb"...)
-   return string(buf)
-}
-
-type Delivery struct {
-   DownloadURL String
-   PackageName string
-   SplitDeliveryData []SplitDeliveryData
+type Header struct {
+   AndroidID uint64
+   SDK int64
    VersionCode uint64
-   AdditionalFile []AppFileMetadata
+   Auth string
 }
 
-type SplitDeliveryData struct {
-   ID String
-   DownloadURL String
+func (t Token) headerVersion(androidID, agent Fixed64) (*Header, error) {
+   val := url.Values{
+      "Token": {t.Token},
+      "service": {"oauth2:https://www.googleapis.com/auth/googleplay"},
+   }.Encode()
+   req, err := http.NewRequest(
+      "POST", "https://android.googleapis.com/auth", strings.NewReader(val),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      return nil, errors.New(res.Status)
+   }
+   var head Header
+   head.AndroidID = uint64(androidID)
+   head.Auth = parseQuery(res.Body).Get("Auth")
+   head.SDK = 9
+   head.VersionCode = uint64(agent)
+   return &head, nil
 }
 
-func (d Delivery) Split(id String) string {
+func (t Token) Header(androidID Fixed64) (*Header, error) {
+   return t.headerVersion(androidID, 9999_9999)
+}
+
+func (t Token) SingleAPK(androidID Fixed64) (*Header, error) {
+   return t.headerVersion(androidID, 8091_9999)
+}
+
+func (h Header) SetAgent(head http.Header) {
    var buf []byte
-   buf = append(buf, d.PackageName...)
-   buf = append(buf, '-')
-   buf = append(buf, id...)
-   buf = append(buf, '-')
-   buf = strconv.AppendUint(buf, d.VersionCode, 10)
-   buf = append(buf, ".apk"...)
-   return string(buf)
+   buf = append(buf, "Android-Finsky (sdk="...)
+   buf = strconv.AppendInt(buf, h.SDK, 10)
+   buf = append(buf, ",versionCode="...)
+   buf = strconv.AppendUint(buf, h.VersionCode, 10)
+   head.Set("User-Agent", string(buf))
 }
 
-func (d Delivery) Download() string {
-   var buf []byte
-   buf = append(buf, d.PackageName...)
-   buf = append(buf, '-')
-   buf = strconv.AppendUint(buf, d.VersionCode, 10)
-   buf = append(buf, ".apk"...)
-   return string(buf)
+// Purchase app. Only needs to be done once per Google account.
+func (h Header) Purchase(app string) error {
+   query := "doc=" + url.QueryEscape(app)
+   req, err := http.NewRequest(
+      "POST", "https://android.clients.google.com/fdfe/purchase",
+      strings.NewReader(query),
+   )
+   if err != nil {
+      return err
+   }
+   h.SetAuth(req.Header)
+   h.SetDevice(req.Header)
+   req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+   LogLevel.Dump(req)
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      return errors.New(res.Status)
+   }
+   return nil
 }
 
 type Message = protobuf.Message
@@ -198,175 +172,4 @@ func OpenToken(elem ...string) (*Token, error) {
 
 func (t Token) Create(elem ...string) error {
    return format.Create(t, elem...)
-}
-
-type NativePlatform map[int64]string
-
-var Platforms = NativePlatform{
-   // com.google.android.youtube
-   0: "x86",
-   // com.miui.weather2
-   1: "armeabi-v7a",
-   // com.kakaogames.twodin
-   2: "arm64-v8a",
-}
-
-func (n NativePlatform) String() string {
-   first := true
-   var buf []byte
-   for key, val := range n {
-      if first {
-         first = false
-      } else {
-         buf = append(buf, '\n')
-      }
-      buf = strconv.AppendInt(buf, key, 10)
-      buf = append(buf, ' ')
-      buf = append(buf, val...)
-   }
-   return string(buf)
-}
-
-type Device struct {
-   AndroidID Fixed64
-}
-
-func OpenDevice(elem ...string) (*Device, error) {
-   return format.Open[Device](elem...)
-}
-
-func (d Device) Create(elem ...string) error {
-   return format.Create(d, elem...)
-}
-
-// These can use default values, but they must all be included
-type Config struct {
-   DeviceFeature []String
-   GlEsVersion Varint
-   GlExtension []String
-   HasFiveWayNavigation Varint
-   HasHardKeyboard Varint
-   Keyboard Varint
-   Navigation Varint
-   ScreenDensity Varint
-   ScreenLayout Varint
-   SystemSharedLibrary []String
-   TouchScreen Varint
-}
-
-var Phone = Config{
-   DeviceFeature: []String{
-      // app.source.getcontact
-      "android.hardware.location.gps",
-      // br.com.rodrigokolb.realdrum
-      "android.software.midi",
-      // com.app.xt
-      "android.hardware.camera.front",
-      // com.clearchannel.iheartradio.controller
-      "android.hardware.microphone",
-      // com.google.android.apps.walletnfcrel
-      "android.software.device_admin",
-      // com.google.android.youtube
-      "android.hardware.touchscreen",
-      "android.hardware.wifi",
-      // com.illumix.fnafar
-      "android.hardware.sensor.gyroscope",
-      // com.madhead.tos.zh
-      "android.hardware.sensor.accelerometer",
-      // com.miHoYo.GenshinImpact
-      "android.hardware.opengles.aep",
-      // com.pinterest
-      "android.hardware.camera",
-      "android.hardware.location",
-      "android.hardware.screen.portrait",
-      // com.sygic.aura
-      "android.hardware.location.network",
-      // com.xiaomi.smarthome
-      "android.hardware.bluetooth",
-      "android.hardware.bluetooth_le",
-      "android.hardware.camera.autofocus",
-      "android.hardware.usb.host",
-      // kr.sira.metal
-      "android.hardware.sensor.compass",
-      // org.thoughtcrime.securesms
-      "android.hardware.telephony",
-      // org.videolan.vlc
-      "android.hardware.screen.landscape",
-   },
-   SystemSharedLibrary: []String{
-      // com.amctve.amcfullepisodes
-      "org.apache.http.legacy",
-      // com.binance.dev
-      "android.test.runner",
-      // com.miui.weather2
-      "global-miui11-empty.jar",
-   },
-   // com.axis.drawingdesk.v3
-   GlEsVersion: 0x9_9999,
-   GlExtension: []String{
-      // com.instagram.android
-      "GL_OES_compressed_ETC1_RGB8_texture",
-      // com.kakaogames.twodin
-      "GL_KHR_texture_compression_astc_ldr",
-   },
-   // com.valvesoftware.android.steam.community
-   TouchScreen: 3,
-}
-
-// A Sleep is needed after this.
-func (c Config) Checkin(platform string) (*Device, error) {
-   checkin := Message{
-      4: Message{ // checkin
-         1: Message{ // build
-            10: Varint(29), // sdkVersion
-         },
-      },
-      14: Varint(3), // version
-      18: Message{ // deviceConfiguration
-         1: c.TouchScreen, // touchScreen
-         2: c.Keyboard, // keyboard
-         3: c.Navigation, // navigation
-         4: c.ScreenLayout, // screenLayout
-         5: c.HasHardKeyboard, // hasHardKeyboard
-         6: c.HasFiveWayNavigation, // hasFiveWayNavigation
-         7: c.ScreenDensity, // screenDensity
-         8: c.GlEsVersion, // glEsVersion
-         11: String(platform), // nativePlatform
-      },
-   }
-   for _, library := range c.SystemSharedLibrary {
-      checkin.Get(18).AddString(9, library)
-   }
-   for _, extension := range c.GlExtension {
-      checkin.Get(18).AddString(15, extension)
-   }
-   for _, name := range c.DeviceFeature {
-      // .deviceConfiguration.deviceFeature
-      checkin.Get(18).Add(26, Message{1: name})
-   }
-   req, err := http.NewRequest(
-      "POST", "https://android.googleapis.com/checkin",
-      bytes.NewReader(checkin.Marshal()),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("Content-Type", "application/x-protobuffer")
-   LogLevel.Dump(req)
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   checkinResponse, err := protobuf.Decode(res.Body)
-   if err != nil {
-      return nil, err
-   }
-   var dev Device
-   // .androidId
-   dev.AndroidID, err = checkinResponse.GetFixed64(7)
-   if err != nil {
-      return nil, err
-   }
-   return &dev, nil
 }
