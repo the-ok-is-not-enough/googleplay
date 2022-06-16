6bd97a65d77466f5c0e59f53688a18695e6b199d
Zombo <srpen6@gmail.com>
2022-06-16 17:07:09 -0500

    +   {"2022-05-18", 1, "com.madhead.tos.zh"},

--- details_test.go
+++ details_test.go
@@ -6,6 +6,31 @@ import (
    "time"
 )
 
+var apps = []appType{
+   {"2021-12-08", 0, "com.amctve.amcfullepisodes"},
+   {"2022-02-02", 2, "com.illumix.fnafar"},
+   {"2022-02-14", 0, "org.videolan.vlc"},
+   {"2022-03-01", 0, "kr.sira.metal"},
+   {"2022-03-16", 2, "com.miHoYo.GenshinImpact"},
+   {"2022-03-17", 0, "com.google.android.apps.walletnfcrel"},
+   {"2022-03-24", 0, "app.source.getcontact"},
+   {"2022-03-24", 1, "com.miui.weather2"},
+   {"2022-04-08", 1, "com.axis.drawingdesk.v3"},
+   {"2022-04-27", 1, "com.sygic.aura"},
+   {"2022-04-29", 1, "com.xiaomi.smarthome"},
+   {"2022-05-05", 0, "com.clearchannel.iheartradio.controller"},
+   {"2022-05-11", 1, "com.supercell.brawlstars"},
+   {"2022-05-12", 0, "com.google.android.youtube"},
+   {"2022-05-13", 0, "com.app.xt"},
+   {"2022-05-15", 2, "com.kakaogames.twodin"},
+   {"2022-05-16", 0, "com.instagram.android"},
+   {"2022-05-16", 1, "com.binance.dev"},
+   {"2022-05-17", 0, "br.com.rodrigokolb.realdrum"},
+   {"2022-05-17", 0, "com.pinterest"},
+   {"2022-05-17", 0, "org.thoughtcrime.securesms"},
+   {"2022-05-18", 1, "com.madhead.tos.zh"},
+}
+
 func TestDetails(t *testing.T) {
    home, err := os.UserHomeDir()
    if err != nil {
@@ -56,30 +81,6 @@ func TestDetails(t *testing.T) {
       }
       time.Sleep(99 * time.Millisecond)
    }
-}
-var apps = []appType{
-   {"2021-12-08", 0, "com.amctve.amcfullepisodes"},
-   {"2022-02-02", 2, "com.illumix.fnafar"},
-   {"2022-02-14", 0, "org.videolan.vlc"},
-   {"2022-03-01", 0, "kr.sira.metal"},
-   {"2022-03-16", 2, "com.miHoYo.GenshinImpact"},
-   {"2022-03-17", 0, "com.google.android.apps.walletnfcrel"},
-   {"2022-03-24", 0, "app.source.getcontact"},
-   {"2022-03-24", 1, "com.miui.weather2"},
-   {"2022-04-08", 1, "com.axis.drawingdesk.v3"},
-   {"2022-04-27", 1, "com.sygic.aura"},
-   {"2022-04-29", 1, "com.xiaomi.smarthome"},
-   {"2022-05-05", 0, "com.clearchannel.iheartradio.controller"},
-   {"2022-05-11", 1, "com.supercell.brawlstars"},
-   {"2022-05-12", 0, "com.google.android.youtube"},
-   {"2022-05-13", 0, "com.app.xt"},
-   {"2022-05-15", 2, "com.kakaogames.twodin"},
-   {"2022-05-16", 0, "com.instagram.android"},
-   {"2022-05-16", 1, "com.binance.dev"},
-   {"2022-05-17", 0, "br.com.rodrigokolb.realdrum"},
-   {"2022-05-17", 0, "com.pinterest"},
-   {"2022-05-17", 0, "org.thoughtcrime.securesms"},
-   {"2022-05-18", 1, "com.madhead.tos.zh"},
 }
 
 type appType struct {
